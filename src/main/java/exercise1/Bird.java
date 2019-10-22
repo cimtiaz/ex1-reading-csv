9ac3855c4381797270bf6b3a894e67dfc15bf3a0
Imtiaz <7255604+cimtiaz@users.noreply.github.com>
2019-10-22 13:49:49 +0200

    Adding constructor comments to Animal sub classes

--- src/main/java/exercise1/Bird.java
+++ src/main/java/exercise1/Bird.java
@@ -6,6 +6,12 @@ package exercise1;
  * @version 2019.10.21
  */
 public class Bird extends Animal {
+    /**
+     * Constructor: Create an instance
+     * @param type A String holding the type
+     * @param name A String holding the name
+     * @param birthYear A String holding the 4-digit year of birth
+     */
     public Bird(String type, String name, String birthYear) {
         super(type, name, birthYear);
     }
--- src/main/java/exercise1/Dog.java
+++ src/main/java/exercise1/Dog.java
@@ -6,6 +6,12 @@ package exercise1;
  * @version 2019.10.21
  */
 public class Dog extends Animal {
+    /**
+     * Constructor: Create an instance
+     * @param type A String holding the type
+     * @param name A String holding the name
+     * @param birthYear A String holding the 4-digit year of birth
+     */
     public Dog(String type, String name, String birthYear) {
         super(type, name, birthYear);
     }
