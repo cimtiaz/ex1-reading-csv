b954deb1fbf791d89c987f3854148cdf6a4c5da7
Imtiaz <7255604+cimtiaz@users.noreply.github.com>
2019-10-22 14:03:10 +0200

    implementing swim method of Swimmer interface

--- src/main/java/exercise1/Fish.java
+++ src/main/java/exercise1/Fish.java
@@ -6,7 +6,21 @@ package exercise1;
  * @version 2019.10.21
  */
 public class Fish extends Animal implements Swimmer {
+    /**
+     * Constructor: Create an instance
+     * @param type A String holding the type
+     * @param name A String holding the name
+     * @param birthYear A String holding the 4-digit year of birth
+     */
     public Fish(String type, String name, String birthYear) {
         super(type, name, birthYear);
+    }
+
+    /**
+     * A method that returns the swimming features
+     * @return The swimming features as a String
+     */
+    public String swim() {
+        return ("I can swim and live in water, but I cannot breath and live for long time in air.");
     }
 }
