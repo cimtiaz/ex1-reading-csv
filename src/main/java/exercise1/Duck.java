96b36785283f519bbb83191fb03107f30bb153c3
Imtiaz <7255604+cimtiaz@users.noreply.github.com>
2019-10-22 14:04:44 +0200

    Changing parent of Duck class as Bird, and implementing swim method of Swimmer interface

--- src/main/java/exercise1/Duck.java
+++ src/main/java/exercise1/Duck.java
@@ -5,8 +5,22 @@ package exercise1;
  * @author Imtiaz Akhtar
  * @version 2019.10.21
  */
-public class Duck extends Animal implements Swimmer {
+public class Duck extends Bird implements Swimmer {
+    /**
+     * Constructor: Create an instance
+     * @param type A String holding the type
+     * @param name A String holding the name
+     * @param birthYear A String holding the 4-digit year of birth
+     */
     public Duck(String type, String name, String birthYear) {
         super(type, name, birthYear);
+    }
+
+    /**
+     * A method that returns the swimming features
+     * @return The swimming features as a String
+     */
+    public String swim() {
+        return ("I can swim and dive in water, but I cannot live in water.");
     }
 }
